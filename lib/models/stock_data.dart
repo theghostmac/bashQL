e50a8f630f21bb560343c51f47207d8a864fe74e
jim-nnamdi <jimsamuel50@gmail.com>
2023-11-04 02:08:12 +0100

    datapoint for stockdata information from mDB

--- lib/models/stock_data.dart
+++ lib/models/stock_data.dart
@@ -1,5 +1,10 @@
+import "dart:io";
+
+import "package:forexql/dataclient/endpoint.dart";
 import "package:json_annotation/json_annotation.dart";
 import "dart:convert";
+import "package:http/http.dart" as http;
+import "dart:async";
 
 part "stock_data.g.dart";
 
@@ -13,4 +18,17 @@ class StockData {
 
   factory StockData.fromJson(Map<String, dynamic> json) =>
       _$StockDataFromJson(json);
+
+  static Future<List<StockData>> fetchStockData() async {
+    var url = Endpoint.uri("/stockdata");
+    var resp = await http.get(url.toString() as Uri);
+    if (resp.statusCode != HttpStatus.ok) throw ("bad response code");
+    var decodedInfo = json.decode(resp.body);
+    if (decodedInfo == null) throw ("empty data response");
+    List<StockData> stockdata = [];
+    for (var res in decodedInfo) {
+      StockData.fromJson(res);
+    }
+    return stockdata;
+  }
 }
